45a0a63261150451aab539d9748ea898a6248db2
Mélina-13 <melina.mouri@epitech.eu>
2025-09-22 16:38:59 +0200

    remove double import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './index.css';  // ou global.css selon ton fichier
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
